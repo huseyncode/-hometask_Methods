bae7f35a585231e63beb3a94daba5ec8bcecc061
huseyncode <huseynmsh@code.edu.az>
2024-06-08 15:50:29 +0400

    first commit

---
+++ #hometask_Methods/Methods.cs
@@ -0,0 +1,80 @@
+﻿public class Methods
+{
+    // Task 1
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    // Task 2
+    public int Square(int a)
+    {
+        return a * a;
+    }
+
+    // Task 3
+    public int SumArray(int[] arr)
+    {
+        int sum = 0;
+        foreach (int num in arr)
+        {
+            sum += num;
+        }
+        return sum;
+    }
+
+    // Task 4
+    public string WelcomeMessage(string name, string surname)
+    {
+        return $"Xoş gəlmisiniz, {name} {surname}!";
+    }
+
+    // Task 5
+    public string ReverseString(string str)
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+    // Task 6
+    public int CheckDivisibleBySeven(int num)
+    {
+        if (num % 7 == 0)
+        {
+            Console.WriteLine("7-ə bölünür");
+            return num;
+        }
+        else
+        {
+            int lower = num - (num % 7);
+            int upper = lower + 7;
+            return (num - lower < upper - num) ? lower : upper;
+        }
+    }
+
+    // Task 7
+    public double Perimeter(double side1, double side2) // For rectangle
+    {
+        return 2 * (side1 + side2);
+    }
+
+    public double Perimeter(double side1, double side2, double side3) // For triangle
+    {
+        return side1 + side2 + side3;
+    }
+
+    // Task 8
+    public int CountSpaces(string sentence)
+    {
+        int count = 0;
+        foreach (char c in sentence)
+        {
+            if (c == ' ')
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ #hometask_Methods/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Methods methods = new Methods();
+
+        int product = methods.Multiply(5, 4);
+        Console.WriteLine($"Product: {product}");
+
+        int square = methods.Square(5);
+        Console.WriteLine($"Square: {square}");
+
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int sum = methods.SumArray(arr);
+        Console.WriteLine($"Sum: {sum}");
+
+        string message = methods.WelcomeMessage("Huseyn", "Sukurov");
+        Console.WriteLine(message);
+
+        string reversed = methods.ReverseString("Salam");
+        Console.WriteLine($"Reversed: {reversed}");
+
+        int result = methods.CheckDivisibleBySeven(49);
+        Console.WriteLine($"Result: {result}");
+
+        double rectanglePerimeter = methods.Perimeter(5.0, 4.0);
+        Console.WriteLine($"Rectangle Perimeter: {rectanglePerimeter}");
+
+        double trianglePerimeter = methods.Perimeter(3.0, 4.0, 5.0);
+        Console.WriteLine($"Triangle Perimeter: {trianglePerimeter}");
+
+        int spaces = methods.CountSpaces("Salam Eleykum");
+        Console.WriteLine($"Spaces: {spaces}");
+    }
+}
